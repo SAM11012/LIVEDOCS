5c2b1ced00e9e9da84383c463781aaa70ac79057
Samyak Meshram <samyakmeshram008@Samyaks-MacBook-Pro-2.local>
2025-05-26 02:45:11 +0530

    added the ignore build errors true

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
-    remotePatterns:[{protocol:'https',hostname:'img.clerk.com'}]
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
+  },
 };
 
 export default nextConfig;
